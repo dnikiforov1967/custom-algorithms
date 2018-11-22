d669afa0e5cfe86b62fc719ecac6339febc6e7a2
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-11-22 20:46:09 +0100

    correct version

--- src/main/java/org/example/custom/algorithms/sort/radix/GenericRadixSort.java
+++ src/main/java/org/example/custom/algorithms/sort/radix/GenericRadixSort.java
@@ -5,53 +5,49 @@
  */
 package org.example.custom.algorithms.sort.radix;
 
-import java.util.Arrays;
-
 /**
  *
  * @author dnikiforov
  */
 public class GenericRadixSort<T extends ByteWrapper> {
-	
-	private final int[] counters = new int[127]; //0..255 by radix
-	
-	public Object[] sort(T[] array, int byteCount) {
-		//Integer consists of 4 bytes in Java. This is universal rule
-		Object[] storage = new Object[array.length];
-		Object[] source = array, target = storage;
-		int index = 0;
-		for(int j = 1; j <= byteCount; j++) {
-			
-			for (int i = 0; i<source.length; i++) {
-				final T xObj = (T)source[i];
-				final byte element = xObj.toByteArray()[byteCount-j];
-				final int position = element;
-				counters[position]++; //Find radix value - byte's AND - in this case let's increase
-				//System.out.println("Position of "+source[i]+" is "+position+", counters "+counters[position]);
-				
-			}
-			for (int i= 1; i<counters.length; i++) counters[i] += counters[i-1];
 
-			for (int i = source.length-1; i>=0; i--) {
-				System.out.println(source[i]);
-			}
-			//for (int i = 0; i<source.length; i++) {
-			final int jIndex = byteCount-j;
-			Arrays.stream(source).forEach((o)->{
-				final T obj = (T)o;
-				final byte[] toByteArray = obj.toByteArray();
-				final byte element = toByteArray[jIndex];
-				counters[element] -= 1;
-				System.out.println("Element "+obj+" in position "+counters[element]);
-				target[counters[element]] = obj;
-			}
-				);	
-			//}	
-			
-			for (int i= 1; i<counters.length; i++) counters[i] = 0;
-			source = target;
-		}
-		return source;
-	}	
-	
+    private final int[] counters = new int[127]; //0..127 by byte value
+
+    public Object[] sort(T[] array, int byteCount) {
+
+        Object[] storage = new Object[array.length];
+        Object[] source = array, target = storage;
+        int index = 0;
+        for (int j = 1; j <= byteCount; j++) {
+
+            final int jIndex = byteCount - j;
+            for (int i = 0; i < source.length; i++) {
+                final T obj = (T) source[i];
+                final byte position = obj.toByteArray()[jIndex];
+                counters[position]++;
+            }
+            
+            for (int i = 1; i < counters.length; i++) {
+                counters[i] += counters[i - 1];
+            }
+
+            for (int i = source.length - 1; i >= 0; i--) {
+                final T obj = (T) source[i];
+                final byte[] toByteArray = obj.toByteArray();
+                final byte position = toByteArray[jIndex];
+                int p = counters[position] -= 1;
+                target[p] = obj;
+            }
+
+            for (int i = 1; i < counters.length; i++) {
+                counters[i] = 0;
+            }
+            
+            Object[] swap = source;
+            source = target;
+            target = swap;
+        }
+        return source;
+    }
+
 }
