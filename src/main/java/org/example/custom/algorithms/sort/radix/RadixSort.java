aa58b837558a39d3459f4c4e9b63b005a3c9315d
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-11-22 13:21:43 +0100

    Radix sort for integers

---
+++ src/main/java/org/example/custom/algorithms/sort/radix/RadixSort.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.example.custom.algorithms.sort.radix;
+
+public class RadixSort {
+
+	int RADIX = 0xFF;
+	int[] counters = new int[256]; //0..255 by radix
+
+	public void sort(int[] A) {
+		//Integer consists of 4 bytes in Java. This is universal rule
+		int[] B = new int[A.length];
+		int[] source, target;
+		int index = 0;
+		for(int j = 1; j <= 4; j++) { //Four steps
+			if (j%2 == 1) {
+				source = A;
+				target = B;
+			}
+			else {
+				source = B;
+				target = A;
+			}
+			for (int i = 0; i<source.length; i++)
+				counters[(source[i] >> 8*(j-1)) & RADIX]++; //Find radix value - byte's AND - in this case let's increase
+			for (int i= 1; i<counters.length; i++) counters[i] += counters[i-1];
+
+			for (int i = source.length-1; i>=0; i--) {
+				index = (source[i] >> 8*(j-1)) & RADIX;
+				counters[index] -= 1;
+				target[counters[index]] = source[i];
+			}	
+			for (int i= 1; i<counters.length; i++) counters[i] = 0;
+		}
+	}
+
+}
