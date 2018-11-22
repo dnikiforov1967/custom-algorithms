a5578b88a43d133804f64da6612fa272bb06ea48
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-11-22 21:36:01 +0100

    New test for special padding convertion

--- src/test/java/org/example/custom/algorithms/sort/radix/GenericRadixUnitTest.java
+++ src/test/java/org/example/custom/algorithms/sort/radix/GenericRadixUnitTest.java
@@ -63,6 +63,59 @@ public class GenericRadixUnitTest {
 	}        
 
     }
+    
+    static class StringWrapperPadded extends ByteWrapper<String> {
+
+        public StringWrapperPadded(String obj, int fixedLength) {
+            super(obj, fixedLength);
+        }
+
+        @Override
+        protected byte[] convert() {
+            final byte[] bytes = obj.getBytes();
+            if (obj.length()<fixedLength) {
+                byte[] full = new byte[fixedLength];
+                int start = fixedLength - bytes.length;
+                for(int i=0; i<bytes.length; i++) {
+                    full[start + i] = bytes[i];
+                }
+                return full;
+            } else {
+                return bytes;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return obj;
+        }
+        
+	@Override
+	public int hashCode() {
+		int hash = 3;
+		hash = 31 * hash + Objects.hashCode(this.obj);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final StringWrapperPadded other = (StringWrapperPadded) obj;
+		if (!Objects.equals(this.obj, other.obj)) {
+			return false;
+		}
+		return true;
+	}        
+
+    }    
 
     public GenericRadixUnitTest() {
     }
@@ -87,7 +140,7 @@ public class GenericRadixUnitTest {
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
-    public void hello() {
+    public void testRadixStringSort() {
         StringWrapper[] array = {
             new StringWrapper("1", 2),
             new StringWrapper("2", 2),
@@ -110,10 +163,40 @@ public class GenericRadixUnitTest {
             };        
 
         GenericRadixSort<StringWrapper> grs = new GenericRadixSort<>();
-        grs.sort(array, 2);
+        array = (StringWrapper[]) grs.sort(array, 2);
 
         assertArrayEquals(expectedArray, array);
 
     }
+    
+    @Test
+    public void testRadixPaddedStringSort() {
+        StringWrapperPadded[] array = {
+            new StringWrapperPadded("1", 2),
+            new StringWrapperPadded("2", 2),
+            new StringWrapperPadded("6", 2),
+            new StringWrapperPadded("10", 2),
+            new StringWrapperPadded("17", 2),
+            new StringWrapperPadded("11", 2),
+            new StringWrapperPadded("13", 2),
+            new StringWrapperPadded("5", 2),};
+        
+        StringWrapperPadded[] expectedArray = {
+            new StringWrapperPadded("1", 2),          
+            new StringWrapperPadded("2", 2),
+            new StringWrapperPadded("5", 2),
+            new StringWrapperPadded("6", 2),
+            new StringWrapperPadded("10", 2),
+            new StringWrapperPadded("11", 2),
+            new StringWrapperPadded("13", 2),
+            new StringWrapperPadded("17", 2),              
+            };        
+
+        GenericRadixSort<StringWrapperPadded> grs = new GenericRadixSort<>();
+        array = (StringWrapperPadded[]) grs.sort(array, 2);
+
+        assertArrayEquals(expectedArray, array);
+
+    }    
 
 }
