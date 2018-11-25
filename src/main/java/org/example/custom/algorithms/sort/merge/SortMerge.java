0583dbc66a6b973f56a498893a873dcfc64cf1fd
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-11-25 19:08:32 +0100

    Sort Merge

---
+++ src/main/java/org/example/custom/algorithms/sort/merge/SortMerge.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.example.custom.algorithms.sort.merge;
+
+import java.util.List;
+
+/**
+ *
+ * @author dima
+ */
+public final class SortMerge<T extends Comparable<T>> {
+    
+    public void sort(final T[] array) {
+        //start from full range
+        sort(array, 0, array.length-1);
+    }
+    
+    void swap(final T[] array, final int i, final int j) {
+        T t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+    
+    public void sort(final T[] array, final int low, final int top) {
+        if (low==top) return;
+        if (low==top-1) {
+            if (array[low].compareTo(array[top])==1) {
+                swap(array,low,top);
+                return;
+            }
+        }
+        //sort 1/2 of piece
+        int middle = (low + top)/2;
+        sort(array, low, middle);
+        sort(array, middle+1, top);
+        Object[] result = new Object[top-low+1];
+        int i=low, j=middle+1, k=0;
+        while(i<=middle || j<=top) {
+            //we have reached limit in first half
+            if (i > middle) {
+                result[k++] = array[j++];
+            } else if (j > top) {
+                result[k++] = array[i++];
+            } else {
+                if (array[i].compareTo(array[j])==1) {
+                    result[k++]=array[j++];
+                } else {
+                    result[k++]=array[i++];
+                }
+            }
+        }
+        for(int m=0, p=low; m<result.length; m++) {
+            array[p++]=(T)result[m];
+        }
+    }    
+    
+}
---
+++ src/test/java/org/example/custom/algorithms/sort/merge/MergeSortUnitTest.java
@@ -0,0 +1,85 @@
+package org.example.custom.algorithms.sort.merge;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dima
+ */
+public class MergeSortUnitTest {
+    
+    public MergeSortUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testSwap() {
+        Integer[] array = {1,2,4,3,5,6};
+        Integer[] expectedArray = {1,2,3,4,5,6};
+        new SortMerge().swap(array, 2, 3);
+        assertArrayEquals(expectedArray, array);
+    }
+    
+    @Test
+    public void testSortEven() {
+        Integer[] array = {1,4,6,2,5,3};
+        Integer[] expectedArray = {1,2,3,4,5,6};
+        new SortMerge().sort(array);
+        assertArrayEquals(expectedArray,array);
+    }    
+    
+    @Test
+    public void testSortOdd() {
+        Integer[] array = {1,7,4,6,2,5,3};
+        Integer[] expectedArray = {1,2,3,4,5,6,7};
+        new SortMerge().sort(array);
+        assertArrayEquals(expectedArray,array);
+    }        
+ 
+    @Test
+    public void testSortOddDoublets() {
+        Integer[] array = {1,6,7,4,5,6,2,5,3};
+        Integer[] expectedArray = {1,2,3,4,5,5,6,6,7};
+        new SortMerge().sort(array);
+        assertArrayEquals(expectedArray,array);
+    }    
+    
+    @Test
+    public void testSortEvenDoublets() {
+        Integer[] array = {1,6,7,4,5,6,2,5,3,1};
+        Integer[] expectedArray = {1,1,2,3,4,5,5,6,6,7};
+        new SortMerge().sort(array);
+        assertArrayEquals(expectedArray,array);
+    }        
+    
+}
