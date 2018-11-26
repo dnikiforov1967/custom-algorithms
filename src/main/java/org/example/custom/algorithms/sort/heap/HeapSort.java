1c782293c2eff856ff2f3a408211e5e523fc4b65
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-11-26 14:03:36 +0100

    Initial (NlogN) heap build

--- src/main/java/org/example/custom/algorithms/sort/heap/HeapSort.java
+++ src/main/java/org/example/custom/algorithms/sort/heap/HeapSort.java
@@ -10,4 +10,54 @@ package org.example.custom.algorithms.sort.heap;
  * @author dima
  */
 public final class HeapSort {
+
+	private HeapSort() {
+	}
+
+	public static <T extends Comparable<T>> void sort(T[] array) {
+
+	}
+
+	static int initialIndex(int idx) {
+		int rest = idx % 2;
+		if (rest == 0) {
+			return idx / 2 - 1;
+		} else {
+			return idx / 2;
+		}
+
+	}
+
+	static <T extends Comparable<T>> void swap(T[] array, int i, int j) {
+		T x = array[i];
+		array[i] = array[j];
+		array[j] = x;
+	}
+	
+	private static <T extends Comparable<T>> void buildHeapNlogN(T[] array, int tail) {
+		//we suppose till tail it's a heap
+		int j = tail;
+		int i = initialIndex(j);
+		while(i>=0) {
+			if (array[i].compareTo(array[j])==-1) {
+				swap(array,i,j);
+				//Top element
+				if (i==0) {
+					break;
+				}				
+				j = i;
+				i = initialIndex(j);
+			} else {
+				break;
+			}
+		}
+		
+	}
+
+	static <T extends Comparable<T>> void buildHeapNlogN(T[] array) {
+		for(int i=1; i<array.length; i++) {
+			buildHeapNlogN(array, i);
+		}
+	}
+
 }
---
+++ src/test/java/org/example/custom/algorithms/sort/heap/HeapSortUnitTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.example.custom.algorithms.sort.heap;
+
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dnikiforov
+ */
+public class HeapSortUnitTest {
+	
+	public HeapSortUnitTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	// TODO add test methods here.
+	// The methods must be annotated with annotation @Test. For example:
+	//
+	@Test
+	public void testIndex() {
+		int i = HeapSort.initialIndex(10);
+		assertEquals(4, i);
+		i = HeapSort.initialIndex(11);
+		assertEquals(5, i);
+	}
+	
+    @Test
+    public void testSwap() {
+        Integer[] array = {1,2,4,3,5,6};
+        Integer[] expectedArray = {1,2,3,4,5,6};
+        HeapSort.swap(array, 2, 3);
+        assertArrayEquals(expectedArray, array);
+    }
+
+    @Test
+    public void testBuildHeapNlogN() {
+        Integer[] array = {4,1,7,12,5,8,2,6};
+        Integer[] expectedArray = {12,7,8,6,5,4,2,1};
+		HeapSort.buildHeapNlogN(array);
+        assertArrayEquals(expectedArray, array);
+    }	
+
+	
+	
+}
