ccf4bd41153e94b9618c2d5e5e1befcc69cf40e0
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-11-27 15:51:50 +0100

    Append some base function for AVL

--- src/main/java/org/example/custom/algorithms/struct/AvlTree.java
+++ src/main/java/org/example/custom/algorithms/struct/AvlTree.java
@@ -9,6 +9,59 @@ package org.example.custom.algorithms.struct;
  *
  * @author dnikiforov
  */
-public class AvlTree {
+public class AvlTree<K extends Comparable<K>, V> {
+
+	private static class Node<K, V> {
+
+		private Node<K, V> left, right;
+		private byte deep = 1;
+
+		private K key;
+		private V value;
+
+		public Node(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+	}
+
+	private Node<K, V> head;
+
+	byte deep(Node<K, V> node) {
+		return (node == null ? 0 : node.deep);
+	}
+
+	int bfactor(Node<K, V> node) {
+		return deep(node.right) - deep(node.left);
+	}
+
+	void fixheight(Node<K, V> node) {
+		byte hl = deep(node.left);
+		byte hr = deep(node.right);
+		node.deep = (byte) ((hl > hr ? hl : hr) + 1);
+	}
+
+	Node<K, V> smallRightTurn(Node<K, V> a) {
+		final Node<K, V> b = a.left;
+		//Keep b.left;
+		//Keep a.right;
+		final Node<K, V> bright = b.right;
+		a.left = bright;
+		b.right = a;
+		return b;
+	}
+
+	Node<K, V> bigRightTurn(Node<K,V> a) {
+		Node<K,V> b = a.left;
+		Node<K,V> c = b.right;
+		//b.left kept
+		//a.right kept
+		b.right = c.left;
+		a.left = b.right;
+		c.left = b;
+		c.right = a;
+		return c;
+	}
 	
 }
