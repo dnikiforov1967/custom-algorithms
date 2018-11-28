de0f64319414021459a74bdf2eb1a02fee94b38e
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-11-28 17:25:04 +0100

    Some balance function (not completed)

--- src/main/java/org/example/custom/algorithms/struct/AvlTree.java
+++ src/main/java/org/example/custom/algorithms/struct/AvlTree.java
@@ -9,81 +9,63 @@ package org.example.custom.algorithms.struct;
  *
  * @author dnikiforov
  */
-public class AvlTree<K extends Comparable<K>, V> {
+public class AvlTree<K extends Comparable<K>, V> extends AbstractTree<K,V>  {
 
-	private static class Node<K, V> {
+	private class Node extends AbstractNode<K,V> {
 
-		private Node<K, V> left, right;
 		private byte deep = 1;
 
-		private K key;
-		private V value;
-
 		public Node(K key, V value) {
-			this.key = key;
-			this.value = value;
+			super(key, value);
 		}
 
 	}
 
-	private Node<K, V> head;
+	private Node head;
 
-	byte deep(Node<K, V> node) {
+	byte deep(Node node) {
 		return (node == null ? 0 : node.deep);
 	}
 
-	int bfactor(Node<K, V> node) {
-		return deep(node.right) - deep(node.left);
+	int bfactor(Node node) {
+		return deep((Node)node.getLeft()) - deep((Node)node.getRight());
 	}
 
-	void fixheight(Node<K, V> node) {
-		byte hl = deep(node.left);
-		byte hr = deep(node.right);
+	void fixheight(Node node) {
+		byte hl = deep((Node)node.getLeft());
+		byte hr = deep((Node)node.getRight());
 		node.deep = (byte) ((hl > hr ? hl : hr) + 1);
 	}
 
-	Node<K, V> smallRightTurn(Node<K, V> a) {
-		final Node<K, V> b = a.left;
-		//Keep b.left;
-		//Keep a.right;
-		final Node<K, V> bright = b.right;
-		a.left = bright;
-		b.right = a;
-		return b;
+	@Override
+	AbstractNode<K, V> smallLeftTurn(AbstractNode<K, V> a) {
+		Node x = (Node)super.smallLeftTurn(a);
+		fixheight(x);
+		fixheight((Node)x.getLeft());
+		return x;
 	}
 
-	Node<K, V> smallLeftTurn(Node<K, V> a) {
-		final Node<K, V> b = a.right;
-		//Keep a.left;
-		//Keep b.right;
-		final Node<K, V> bleft = b.left;
-		a.right = bleft;
-		b.left = a;
-		return b;
+	@Override
+	AbstractNode<K, V> smallRightTurn(AbstractNode<K, V> a) {
+		Node x = (Node)super.smallRightTurn(a);
+		fixheight(x);
+		fixheight((Node)x.getRight());
+		return x;
 	}
-
-	Node<K, V> bigRightTurn(Node<K, V> a) {
-		Node<K, V> b = a.left;
-		Node<K, V> c = b.right;
-		//b.left kept
-		//a.right kept
-		b.right = c.left;
-		a.left = b.right;
-		c.left = b;
-		c.right = a;
-		return c;
-	}
-
-	Node<K, V> bigLeftTurn(Node<K, V> a) {
-		Node<K, V> b = a.right;
-		Node<K, V> c = b.left;
-		//a.left kept
-		//b.right kept
-		a.right = c.left;
-		b.left = c.right;
-		c.left = a;
-		c.right = b;
-		return c;
-	}
+	
+	Node balance(Node p)
+	{
+		fixheight(p); //Calc new height
+		if( bfactor(p)== -2 ) //left - right == -2
+		{
+			if( bfactor((Node)p.getRight()) <= 0 ) {
+				p = (Node)smallLeftTurn(p);
+			} else {
+				p = (Node)bigLeftTurn(p);
+			}	
+		}
+		//TO DO mirror case
+		return p; // No balance
+	}	
 
 }
